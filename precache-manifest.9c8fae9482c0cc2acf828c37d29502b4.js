0376f04184db928f92c84e299ed641f22535982d
David Jeyachandran <davidjeyachandran@gmail.com>
2020-01-07 18:54:50 -0500

    Updates

---
+++ precache-manifest.9c8fae9482c0cc2acf828c37d29502b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31f5c837d1be12b94a17c4bfcf18c41e",
+    "url": "/race2rocks/index.html"
+  },
+  {
+    "revision": "729bec60e137dde16875",
+    "url": "/race2rocks/static/css/main.c34975af.chunk.css"
+  },
+  {
+    "revision": "660a99020d8f1efcd207",
+    "url": "/race2rocks/static/js/2.7275ee03.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/race2rocks/static/js/2.7275ee03.chunk.js.LICENSE"
+  },
+  {
+    "revision": "729bec60e137dde16875",
+    "url": "/race2rocks/static/js/main.62398b7e.chunk.js"
+  },
+  {
+    "revision": "174bbbbd4b32835d9b0e",
+    "url": "/race2rocks/static/js/runtime-main.3324b08f.js"
+  }
+]);
