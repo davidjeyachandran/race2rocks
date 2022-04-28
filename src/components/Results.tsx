6914116b88ba14a68067fef0338cf0ddf287388c
David Jeyachandran <davidjeyachandran@gmail.com>
2022-04-28 21:26:04 +1000

    Add MUI and update App and Result

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,37 +1,38 @@
-import React from 'react'
-import Row from './Row'
+import { Container } from '@mui/material';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
 
 interface PropTypes {
-  data: {
-    Name: string;
-    Time: string;
-    Category: string;
-    Year: number
-  }[]
+    data: {
+        Name: string;
+        Time: string;
+        Category: string;
+        Year: number
+    }[]
 }
 
 function Results({ data }: PropTypes) {
-  return (
-    <div>
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Time</th>
-            <th>Category</th>
-            <th>Year</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map(item =>
-            <Row key={item.Name + item.Time + item.Year} data={item} />
-          )}
-        </tbody>
-      </table>
 
+    const dataWithID = data.map((item) => ({ ...item, id: (item.Name + item.Time + item.Year) }))
 
-    </div>
-  )
+    const columns: GridColDef[] = [
+        { field: 'id', headerName: 'ID', width: 1 },
+        { field: 'Name', headerName: 'Name', width: 150 },
+        { field: 'Time', headerName: 'Time', width: 90 },
+        { field: 'Category', headerName: 'Category', width: 100 },
+        { field: 'Year', headerName: 'Category', width: 80 },
+
+    ];
+
+    return (
+        <Container sx={{ height: '600px' }}>
+            <DataGrid
+                rows={dataWithID}
+                columns={columns}
+                pageSize={30}
+                rowsPerPageOptions={[21]}
+            />
+        </Container>
+    )
 }
 
 export default Results
