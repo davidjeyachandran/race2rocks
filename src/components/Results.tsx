ddfa143617807d48403b841d1b5d4675eb410e3e
David Jeyachandran <davidjeyachandran@gmail.com>
2022-04-30 18:16:34 +1000

    Style DataGrid

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,5 +1,6 @@
+import { useState } from 'react'
 import { Container } from '@mui/material';
-import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import { DataGrid, GridColDef, GridRowParams, MuiEvent } from '@mui/x-data-grid';
 
 interface PropTypes {
     data: {
@@ -11,25 +12,41 @@ interface PropTypes {
 }
 
 function Results({ data }: PropTypes) {
+    const [pageSize, setPageSize] = useState(20);
 
     const dataWithID = data.map((item) => ({ ...item, id: (item.Name + item.Time + item.Year) }))
+    const handleRowClick = (params: GridRowParams) => {
+        console.log(params)
+    }
+
+    const handlePageSizeChange = (pageSize: number) => {
+        setPageSize(pageSize);
+
+    };
+
 
     const columns: GridColDef[] = [
-        { field: 'id', headerName: 'ID', width: 1 },
-        { field: 'Name', headerName: 'Name', width: 150 },
-        { field: 'Time', headerName: 'Time', width: 90 },
+        { field: 'Name', headerName: 'Name', width: 210 },
+        { field: 'Time', headerName: 'Time', width: 100 },
         { field: 'Category', headerName: 'Category', width: 100 },
-        { field: 'Year', headerName: 'Category', width: 80 },
+        { field: 'Year', headerName: 'Year', width: 80 },
 
     ];
 
     return (
-        <Container sx={{ height: '600px' }}>
+        <Container sx={{ height: '800px' }}>
             <DataGrid
+                sx={{
+                    ".MuiTablePagination-selectLabel, .MuiTablePagination-displayedRows": {
+                        marginBottom: 0,
+                    }
+                }}
                 rows={dataWithID}
                 columns={columns}
-                pageSize={30}
-                rowsPerPageOptions={[21]}
+                pageSize={pageSize}
+                rowsPerPageOptions={[20, 50, 100]}
+                onRowClick={handleRowClick}
+                onPageSizeChange={handlePageSizeChange}
             />
         </Container>
     )
