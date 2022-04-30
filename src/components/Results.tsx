ad5b37e11f7179333abc0fb3339b499239475148
David Jeyachandran <davidjeyachandran@gmail.com>
2022-04-30 21:37:14 +1000

    Change DataGrid styling

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,6 +1,5 @@
-import { useState } from 'react'
 import { Container } from '@mui/material';
-import { DataGrid, GridColDef, GridRowParams, MuiEvent } from '@mui/x-data-grid';
+import { DataGrid, GridColDef, GridRowParams } from '@mui/x-data-grid';
 
 interface PropTypes {
     data: {
@@ -12,43 +11,30 @@ interface PropTypes {
 }
 
 function Results({ data }: PropTypes) {
-    const [pageSize, setPageSize] = useState(20);
-
     const dataWithID = data.map((item) => ({ ...item, id: (item.Name + item.Time + item.Year) }))
     const handleRowClick = (params: GridRowParams) => {
         console.log(params)
     }
 
-    const handlePageSizeChange = (pageSize: number) => {
-        setPageSize(pageSize);
-
-    };
-
-
     const columns: GridColDef[] = [
-        { field: 'Name', headerName: 'Name', width: 210 },
-        { field: 'Time', headerName: 'Time', width: 100 },
-        { field: 'Category', headerName: 'Category', width: 100 },
+        { field: 'Name', headerName: 'Name', width: 180 },
+        { field: 'Time', headerName: 'Time', width: 80 },
+        { field: 'Category', headerName: 'Category', width: 80 },
         { field: 'Year', headerName: 'Year', width: 80 },
 
     ];
 
     return (
-        <Container sx={{ height: '800px' }}>
-            <DataGrid
-                sx={{
-                    ".MuiTablePagination-selectLabel, .MuiTablePagination-displayedRows": {
-                        marginBottom: 0,
-                    }
-                }}
-                rows={dataWithID}
-                columns={columns}
-                pageSize={pageSize}
-                rowsPerPageOptions={[20, 50, 100]}
-                onRowClick={handleRowClick}
-                onPageSizeChange={handlePageSizeChange}
-            />
-        </Container>
+        <div style={{ display: 'flex', height: '100%', minHeight: '200px' }}>
+            <div style={{ flexGrow: 1 }}>
+                <DataGrid
+                    rows={dataWithID}
+                    columns={columns}
+                    pageSize={50}
+                    onRowClick={handleRowClick}
+                />
+            </div>
+        </div>
     )
 }
 
