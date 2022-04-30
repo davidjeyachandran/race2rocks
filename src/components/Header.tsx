849573c59324955812ecd1adf6fa3f2c90f6a692
David Jeyachandran <davidjeyachandran@gmail.com>
2022-04-30 21:36:29 +1000

    Add Container

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,4 @@
+import { Container } from '@mui/material';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -8,9 +9,11 @@ export default function Header() {
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static">
                 <Toolbar>
-                    <Typography variant="h4" component="h1" sx={{ flexGrow: 1, textAlign: 'center' }}>
-                        Race to the Rocks
-                    </Typography>
+                    <Container maxWidth="sm" >
+                        <Typography variant="h4" component="h1" sx={{ flexGrow: 1 }}>
+                            Race to the Rocks
+                        </Typography>
+                    </Container>
                 </Toolbar>
             </AppBar>
         </Box>
