4b9425a508ae54cffa46d0c3bfb485b060dca61b
David Jeyachandran <davidjeyachandran@gmail.com>
2022-05-29 17:11:59 +1000

    Rewrite code for practice

--- src/App.tsx
+++ src/App.tsx
@@ -1,86 +1,69 @@
-import React, { useState, useEffect } from 'react';
-import { getDataFromServer } from './utilities'
-import Results from './components/Results'
-import SearchForm from './components/SearchForm'
-import { pluck } from './utilities'
-import ChartLine from './components/ChartLine'
-import { sortByTime } from './sort'
 import { Container } from '@mui/material';
-import Header from './components/Header';
+import { useState, useEffect } from 'react';
+import Header from './components/Header'
+import SearchForm from './components/SearchForm';
+import { pluck } from './utilities';
+import { sortByTime } from './sort'
+import Results from './components/Results';
+import useLoadData from './hooks/useLoadData';
+
+const ENDPOINT = 'data.json'
 
 function App() {
-    const [data, setData] = useState<RunEntry[]>([]);
-    const [filteredData, setFilteredData] = useState<RunEntry[]>([]);
-    const [years, setYears] = useState<string[]>([]);
+    const [filteredData, setFilteredData] = useState<RunDataType[]>([]);
+    const [years, setYears] = useState<string[]>(['All']);
     const [categories, setCategories] = useState<string[]>([]);
-    const [isOnePerson, setIsOnePerson] = useState<boolean>(false);
 
-    const DATA_LOCATION = 'data.json'
+    const [data, isLoading] = useLoadData(ENDPOINT)
+
+    function filterYear(yearSelected: number) {
+        if (yearSelected < 0) {
+            setFilteredData(data)
+            return
+        }
+        const filtered = data.filter(item => item.year === yearSelected)
+        setFilteredData(filtered)
+    }
+
+    function filterName(search: string) {
+        const filtered = data.filter(item =>
+            item.name.toLowerCase().includes(search.toLowerCase())
+        )
+        setFilteredData(filtered)
+    }
+
+    function filterCategory(category: string) {
+        if (category === '') {
+            setFilteredData(data)
+            return
+        }
+        const filtered = data.filter(item =>
+            item.category === category
+        )
+        setFilteredData(filtered)
+    }
 
     useEffect(() => {
-        getDataFromServer(DATA_LOCATION)
-            .then(data => {
-                setData(data)
-                setFilteredData(data.sort(sortByTime))
-                setYears(pluck(data, 'Year'))
-                let categoriesData = pluck(data, 'Category')
-                categoriesData.unshift('All')
-                setCategories(categoriesData)
-
-            })
-    }, [])
-
-    const filterName = (name: string): void => {
-        let filteredData: RunEntry[] = data.filter((item: RunEntry) =>
-            item.Name.toLowerCase().includes(name.toLowerCase())
-        )
-        setFilteredData(filteredData)
-
-        let nameList = pluck(filteredData, 'Name')
-        if (nameList.length === 1) {
-            setIsOnePerson(true)
-        } else {
-            setIsOnePerson(false)
-        }
-    }
-
-    const setYear = (year: number): void => {
-        let filteredData = data.filter((item: RunEntry) =>
-            item.Year === year
-        )
-        setFilteredData(filteredData)
-        setIsOnePerson(false)
-    }
-
-    const setCategory = (category: string): void => {
-        if (category === 'All') {
-            setFilteredData(data)
-        } else {
-            let newFilteredData = data.filter((item: RunEntry) =>
-                item.Category === category
-            )
-            setFilteredData(newFilteredData)
-            setIsOnePerson(false)
-        }
-    }
+        setFilteredData(data.sort(sortByTime))
+        setYears(['All'].concat(pluck(data, 'year')))
+        setCategories(['All'].concat(pluck(data, 'category')))
+    }, [data])
 
     return (
-        <React.Fragment>
-            <Header />
+        <>
+            <Header title='Race 2 the Rocks' />
             <Container maxWidth="sm" >
                 <SearchForm
                     years={years}
-                    setYear={setYear}
-                    filterName={filterName}
                     categories={categories}
-                    setCategory={setCategory}
+                    filterName={filterName}
+                    filterYear={filterYear}
+                    filterCategory={filterCategory}
                 />
-                {isOnePerson ?
-                    <ChartLine filteredData={filteredData} /> :
-                    ''}
-                <Results data={filteredData} />
+                {isLoading ? <p>Loading...</p>
+                    : <Results data={filteredData} />}
             </Container>
-        </React.Fragment>
+        </>
     );
 }
 
--- src/components/ChartLine.tsx
+++ src/components/ChartLine.tsx
@@ -4,41 +4,41 @@ import { pluck, getSeconds } from '../utilities'
 import { sortByYear } from '../sort'
 
 interface ChartLineProps {
-  filteredData: RunEntry[]
+    filteredData: RunDataType[]
 }
 
 function ChartLine({ filteredData }: ChartLineProps) {
 
-  let times = filteredData.sort(sortByYear).map(item => {
-    let time = getSeconds(item.Time) / 60
-    return Math.round(time * 1000) / 1000
-  })
+    let times = filteredData.sort(sortByYear).map(item => {
+        let time = getSeconds(item.time) / 60
+        return Math.round(time * 1000) / 1000
+    })
 
 
-  const data = {
-    labels: pluck(filteredData, 'Year'),
-    datasets: [
-      {
-        label: filteredData[0].Name,
-        backgroundColor: 'rgba(255,99,132,0.2)',
-        borderColor: 'rgba(255,99,132,1)',
-        borderWidth: 1,
-        hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-        hoverBorderColor: 'rgba(255,99,132,1)',
-        data: times
-      }
-    ]
-  };
+    const data = {
+        labels: pluck(filteredData, 'Year'),
+        datasets: [
+            {
+                label: filteredData[0].name,
+                backgroundColor: 'rgba(255,99,132,0.2)',
+                borderColor: 'rgba(255,99,132,1)',
+                borderWidth: 1,
+                hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+                hoverBorderColor: 'rgba(255,99,132,1)',
+                data: times
+            }
+        ]
+    };
 
-  return (
-    <>
-      <Line
-        data={data}
-        width={100}
-        height={50}
-      />
-    </>
-  )
+    return (
+        <>
+            <Line
+                data={data}
+                width={100}
+                height={50}
+            />
+        </>
+    )
 }
 
 export default ChartLine
