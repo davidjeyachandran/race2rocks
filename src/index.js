e174b2e8c7e66bef12646baa3a51f2eff3874be3
David Jeyachandran <david.jeyachandran@sonder.io>
2022-06-04 21:51:17 +1000

    Add Cypress

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ThemeProvider } from '@mui/material/styles'
-import theme from './theme'
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { CssBaseline } from '@mui/material';
 
 const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
+    document.getElementById('root')
 );
 root.render(
     <React.StrictMode>
