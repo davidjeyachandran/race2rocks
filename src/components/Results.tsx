d17441d3fef4764988b63cdb8f3b65117ce69bde
David Jeyachandran <davidjeyachandran@gmail.com>
2023-01-08 18:59:43 +1100

    Change tab size

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -7,35 +7,35 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
 interface ResultsProps {
-    data: RunDataType[]
+  data: RunDataType[]
 }
 export default function Results(props: ResultsProps) {
-    const { data } = props
-    return (
-        <>
-            <TableContainer component={Paper}>
-                <Table aria-label="Results table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Name</TableCell>
-                            <TableCell align="right">Time</TableCell>
-                            <TableCell align="right">Year</TableCell>
-                            <TableCell align="right">Category</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
+  const { data } = props
+  return (
+    <>
+      <TableContainer component={Paper}>
+        <Table aria-label="Results table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell align="right">Time</TableCell>
+              <TableCell align="right">Year</TableCell>
+              <TableCell align="right">Category</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
 
-                        {data.map((row) => (
-                            <TableRow key={row.name + row.time + row.year} >
-                                <TableCell component="th" scope="row">{row.name}</TableCell>
-                                <TableCell align="right">{row.time}</TableCell>
-                                <TableCell align="right">{row.year}</TableCell>
-                                <TableCell align="right">{row.category}</TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-        </>
-    )
+            {data.map((row) => (
+              <TableRow key={row.name + row.time + row.year} >
+                <TableCell component="th" scope="row">{row.name}</TableCell>
+                <TableCell align="right">{row.time}</TableCell>
+                <TableCell align="right">{row.year}</TableCell>
+                <TableCell align="right">{row.category}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
+  )
 }
